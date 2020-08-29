29427cfa1b64d0050a7cfb7417cb01fa15e391e7
soorya nath <56500311+SooryanathIT@users.noreply.github.com>
2020-08-29 12:22:06 +0530

    Update solution.py

--- Assignment-1/solution.py
+++ Assignment-1/solution.py
@@ -24,3 +24,64 @@ def get_entropy_of_dataset(df):
         
     
     return entropy
+
+
+
+def get_entropy_of_attribute(df,attribute):
+
+	
+    empty={}   #an empty dict to store the categories within an attr column.
+    
+    target = df.columns[-1]  #the target column name
+    
+    print('target: ',target)
+
+    for records in range(len(df)): #iterate the df row by row
+        
+        category = df.iloc[records][attribute] #getting the value of attribute for current row
+        
+        target_value = df.iloc[records][target] #getting the target value for that record
+                                    
+        if category not in empty:  #if that category was not visited
+
+            empty[category]={}  #make a key with category name and value is again a dict
+
+            empty[category][target_value]=1 
+
+        else: #if category already there 
+
+            if target_value not in empty[category]: 
+
+                 empty[category][target_value]=1
+
+            else:
+                empty[category][target_value]+=1
+                
+                
+    #every thing is fine now 
+    
+    print(empty)
+    
+    res = 0
+
+    for category in empty:
+
+        data_per_category = empty[category]
+
+        p = list(data_per_category.values())
+
+        dr = sum(p)
+
+        entropy=0
+
+        for vals in p:
+
+            entropy += -1*(vals/dr)*np.log2(vals/dr)
+
+        res = res + dr*entropy/len(df)
+
+    
+    
+    
+
+    return abs(res)
